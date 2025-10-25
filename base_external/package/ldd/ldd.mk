a7a1c21fa2aaa000c2493fc6cb8ca79e78d9344e
Bigeng Wang <bw2482@columbia.edu>
2025-10-24 20:59:03 -0700

    A seems okay version with ldd compiled with recursive make command for module make.

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,30 @@
+
+##############################################################
+#
+# AESD-ASSIGNMENTS
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 7 ldd git contents
+LDD_VERSION = 'b82ea595710180744c47ebb96926335dc7aff223'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = 'git@github.com:bigengwang2482/assignment7-aesdbigengwang2482.git'
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+LDD_MODULE_SUBDIRS = misc-modules scull
+
+define LDD_BUILD_CMDS 
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(LINUX_DIR) M=$(@D) modules
+endef
+
+# TODO add your misc-modules and scull components to the installation steps below
+define LDD_INSTALL_TARGET_CMDS	
+	 $(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/bin
+	 $(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/bin
+	 $(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/bin
+	 $(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/bin
+endef
+
+$(eval $(generic-package))
